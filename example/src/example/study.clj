0c8c1ce6eb39f7905ce50b615faeeb6776afd91a
javajigi <javajigi@gmail.com>
2013-10-01 18:05:44 +0900

    create example project

---
+++ example/src/example/study.clj
@@ -0,0 +1,33 @@
+(ns example.study
+  (:use compojure.core))
+
+(defn hello [name] (str "Hello, " name))
+
+(hello "Park")
+
+(hello "Cloure")
+
+(+ 3 4)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (str x "안녕 여러분."))
+
+(foo "하이요")
+
+(def brown [0 1 2 3 4 5])
+(def blue (pop brown))
+(def green (pop brown))
+
+blue
+green
+
+
+
+
+
+
+
+
+
